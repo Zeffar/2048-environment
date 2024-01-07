739d5e65ae5e256c90f67644cc22cc4f0533dc76
Julie <rotaru.iulia.maria@gmail.com>
2024-01-07 12:30:11 +0200

    added new functionalities + user interface

--- beam_search.cpp
+++ beam_search.cpp
@@ -25,10 +25,6 @@ void generate_2(int board[4][4]);
 int make_generations(node out[], node in[], int width);
 int make_children(node out[], node in[], int width);
 
-bool gen_cmp(node a, node b){
-    return (a.score < b.score);
-}
-
 bool child_cmp(node a, node b) {
     return (a.score > b.score);
 }
@@ -43,13 +39,13 @@ void debug(int board[4][4])
 }
 int main(){
     // srand(time(0)); //for the random function
-    int tree_depth=5, tree_width=200;
+    int tree_depth=5, tree_width=64;
     //here I should create the tree using two vectors with min max beam search and then choose the children using the eval function
     int result = make_tree("C:/2048_env/2048-tree/tree.txt",tree_depth,tree_width);
 
     cout<<to_string(result); 
 
-    return 1;
+    return 0;
 }
 
 
@@ -58,83 +54,94 @@ int make_tree(string b, int depth, int width) {
 
     //2 rows for list of current states of the board and for list of board with the 2s generated on them
     node children[width*5];
-    node generations[width*17];
+    node new_children[width*5];
 
     int x; 
     int child_nr = 0; //to count how many children of the big 4 are valid
 
     ifstream brd(b);
     int board[4][4];
-
+    int orig[4][4];
 
     for (int i = 0; i < 4; ++i)  //read board from file 
         for (int j = 0; j < 4; ++j) {
             brd>>x;
             if (x)
-                board[i][j]=static_cast<int>(log2(x));//since all values are powers of 2, save the log
-            else board[i][j]=0;
+                orig[i][j]=static_cast<int>(log2(x));//since all values are powers of 2, save the log
+            else orig[i][j]=0;
         }
 
-    for(int i=0; i<4; ++i) {
-        x = next_board(children[child_nr].board,board,i); //will write in 'tree[0][child_nr].board', the board for the very first node in the first row of the tree, the modified 'board' after move '0' and x will verify if the fct outputed true or false
-        if (x){
-            children[child_nr].root_move = i; //goes ot the next node of the first row of the tree
-            children[child_nr].score = evaluate(children[child_nr].board);
-            child_nr++;
-        } 
-    }
-    sort(children, children + child_nr + 1, child_cmp);
-    int best_move = children[0].root_move;
-
+    
+    
+    int rounds = 100;
+    int freq[4]={0, 0, 0, 0};
     //child_nr will keep the number of valid root_moves, ex 3 means 3 valid root_moves
     int number_of_generations = 0;
-    for (int r=0; r<depth; ++r) { //every row
-        number_of_generations = make_generations(generations, children, min(child_nr, width/20)); //generated all possible generations
-        // sort the generations increasingly
-        sort(generations, generations + number_of_generations+1, child_cmp);
-        // generate all possible moves
-        child_nr = make_children(children, generations, min(number_of_generations, width));
-        if(!child_nr) break;
-        // sort all moves decreasingly
+    int best_move=0;
+    while(rounds--){
+        for(int i=0; i<4; i++)
+            for(int j=0; j<4; j++)
+                board[i][j]=orig[i][j];
+        child_nr=0;
+        
+
+        for(int i=0; i<4; ++i) {
+            x = next_board(children[child_nr].board,board,i); //will write in 'tree[0][child_nr].board', the board for the very first node in the first row of the tree, the modified 'board' after move '0' and x will verify if the fct outputed true or false
+            if (x){
+
+                children[child_nr].root_move = i; //goes ot the next node of the first row of the tree
+                children[child_nr].score = evaluate(children[child_nr].board);
+                if(children[child_nr].score < 0) continue;
+                child_nr++;
+            } 
+        }
         sort(children, children + child_nr, child_cmp);
-        best_move=children[0].root_move;
+        best_move = children[0].root_move;
+        cerr<<best_move<<" ";
+
+        for (int r=0; r<depth; ++r) { 
+            // generate all possible moves
+            child_nr = make_children(new_children, children, min(child_nr, width));
+            if(!child_nr) break;
+            // sort all moves decreasingly
+            sort(new_children, new_children + child_nr, child_cmp);
+            best_move=children[0].root_move;
+
+            child_nr = make_children(children, new_children, min(child_nr, width));
+            if(!child_nr) break;
+            // sort all moves decreasingly
+            sort(children, children + child_nr, child_cmp);
+            best_move=children[0].root_move;
+
+        }
+        freq[best_move]++;
+
     }
+    int maxi=-1;
+    int rez=0;
+    for (int i=0; i<4; i++)
+        maxi=max(maxi, freq[i]);
+    for (int i=0; i<4; i++) if(freq[i]==maxi){
+        rez=i;
+        break;
+    }
+
     brd.close();
-    return best_move;
+    return rez;
     //return best found move
     
-}
-
-int make_generations(node out[], node in[], int width){ 
-    int index_in=0, index_out=0;
-    
-    for( ; index_in < width; ++index_in){
-        for(int i=0; i<4; ++i)
-            for(int j=0; j<4; ++j) {
-                if (in[index_in].board[i][j] == 0) {
-                    for(int ii=0; ii<4; ++ii)
-                        for(int jj=0; jj<4; ++jj)
-                            out[index_out].board[ii][jj]=in[index_in].board[ii][jj];
-                    out[index_out].board[i][j]=2;
-                    out[index_out].root_move = in[index_in].root_move;
-                    out[index_out].score=evaluate(out[index_out].board);
-                    if(out[index_out].score < 5) continue;
-                    index_out++;
-                }
-            }
-    }
-    return index_out;
 }
 int make_children(node out[], node in[], int width) {
 
     int index_in=0, index_out=0;
     for( ; index_in < width; ++index_in) {
-        if(in[index_in].score==0) return index_out;
+        if(in[index_in].score<1) return index_out;
         for(int i=0; i<4; ++i) {
             int ok=0;
             ok = next_board(out[index_out].board, in[index_in].board, i); 
             if (ok) {
                 out[index_out].score = evaluate(out[index_out].board);
+                if(out[index_out].score < 0) continue;
                 out[index_out].root_move = in[index_in].root_move;
                 index_out++;
             }   
@@ -156,7 +163,6 @@ void generate_2(int board[4][4]){
             }
             counter++;//count at which square we are
         }
-
     int random = rand() % nr_places; //will give a random nr in range of the empty spaces
     board[free_places[random]/4][free_places[random]%4] = 2; //said square will become 2
 }
@@ -171,13 +177,13 @@ int evaluate(int board[4][4]){
         bool canMove=next_board(aux, board, i);
         if (canMove) ok=1;
     }
-    if(!ok) return 1;
+    if(!ok) return -10;
 
 	int w_matrix[4][4]= {    //snake-like pattern for the values, aims to have the highest number in the down left corner
-	         {0, 0, 0, 0},
+	         {-4, -3, -2, -1},
              {1, 2, 3, 4},
              {8, 7, 6, 5},
-             {9, 10, 11, 12}
+             {9, 10, 11, 15}
     };
 
     for (int i=0; i<4; ++i)
@@ -204,8 +210,9 @@ bool next_board(int o_board[4][4], int i_board[4][4], int move){
         case 2: move_down(legal_move, o_board); break;
         case 3: move_left(legal_move, o_board); break;
     }
-
-    return legal_move;
+    if(!legal_move) return 0;
+    generate_2(o_board);
+    return 1;
 }
 
 //move functions
