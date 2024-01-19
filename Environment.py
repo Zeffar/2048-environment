676e490a16c8b515acb1979b9d0655f7826ed4b7
denisaaab <bucur.denisa@gmail.com>
2024-01-19 09:40:32 +0200

    Added calculate_reward function

--- Environment.py
+++ Environment.py
@@ -9,7 +9,7 @@ class Environment_2048:
     def __init__(self, size: int = 4) -> None:
         """
         ::size:: -> row/column length 
-        ::done:: -> inicates if the game is over
+        ::done:: -> indicates if the game is over
         ::state:: -> n x n matrix containing the current state of the game
         ::action_space :: -> class object that contains the possible actions
         """
@@ -43,16 +43,15 @@ class Environment_2048:
             info = game_state
             self.done = True
 
-        reward = self.calculate_reward(next_state)
+        reward = self.calculate_reward(self.state, next_state)
         self.state = next_state
         return next_state, reward, self.done, info
 
     def get_next_state(self, action):
         """
         This functions checks if the action is "legal".
-        An action is legal when tiles merge 
+        An action is legal when tiles merge.
         If the action is ilegal, the state remains the same
-
         """
         commands = {
             0: logic.up,
@@ -72,10 +71,75 @@ class Environment_2048:
         # TODO -> optional
         pass
 
-    def calculate_reward(self, next_state):
-        # TODO -> mandatory
-        pass
+    def calculate_reward(self, old_state, next_state):
+        """
+        Calculates the reward/penalty for taking a specific action
+        """
+        reward = 0
+        
+        # Reward for increase in score (sum of tiles)
+        reward += self.calculate_score_increase(old_state, next_state)
 
+        # Reward for tile merges
+        reward += self.calculate_tile_merges(old_state, next_state)
+
+        # Bonus reward for reaching higher tiles
+        max_tile = max(max(row) for row in next_state)
+        if max_tile >= 512:
+            reward += max_tile // 512
+
+        # Reward for game progression
+        if not self.done:  
+            reward += 0.1
+
+        # Rewards based on board evaluation
+        reward += self.evaluate_board(next_state) - self.evaluate_board(old_state)
+
+        # Penalty for non-effective moves
+        if np.array_equal(old_state, next_state):
+            reward -= 1
+
+        game_state = logic.game_state(next_state)
+        # Penalty for losing game
+        if game_state == "lose":
+            reward -= 2048
+
+        return reward
+
+    def calculate_score_increase(self, old_state, next_state):
+        """
+        This functions calculates the difference in score between given states.
+        The score of a state is calculated as the sum of its tiles
+        """
+        old_score = sum(sum(row) for row in old_state)
+        next_score = sum(sum(row) for row in next_state)
+        return next_score - old_score
+
+    def calculate_tile_merges(self, old_state, next_state):
+        """
+        This functions iterates through the board and for each tile that has changed,
+        adds the difference in value to the merges_reward
+        """
+        merges_reward = 0
+        for i in range(self.size):
+            for j in range(self.size):
+                if next_state[i][j] != old_state[i][j]:
+                    merges_reward += next_state[i][j] - old_state[i][j]
+        return merges_reward
+
+    def evaluate_board(self, state):
+        """
+        This functions evaluates a given board state, calculating a score based on 
+        the values of the tiles and their positions.
+        A weighted matrix weight_matrix is used to give different weights to different positions on the board.
+        """
+        score = 0
+        weight_matrix = [[0, 1, 2, 3], [7, 6, 5, 4], [8, 9, 10, 11], [15, 14, 13, 12]]
+        for i in range(4):
+            for j in range(4):
+                if state[i][j] != 0:
+                    score += np.log2(state[i][j]) * weight_matrix[i][j]
+        return score
 
 class Action_Space_2048:
     def __init__(self, n: int = 4) -> None:
